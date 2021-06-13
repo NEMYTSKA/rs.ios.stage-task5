c5a63df9aaa8b76cdbd739173626971a9ec43b33
ILLIA NEMYTSKA <78763502+NEMYTSKA@users.noreply.github.com>
2021-06-14 01:26:25 +0300

    fix: Ex 2 & 1

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -2,7 +2,33 @@ import Foundation
 
 class StockMaximize {
 
+    
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        
+        guard prices.count > 1 else { return 0 }
+        
+        var myPrices = prices
+        var result = 0
+        
+        func filtrMyPrices(_ value: inout [Int]) {
+            for i in value {
+                if i == value.max() {
+                    value.remove(at: 0)
+                } else {
+                    break
+                }
+            }
+        }
+        
+        filtrMyPrices(&myPrices)
+        
+        
+        while myPrices.count > 1  {
+            result += myPrices.max()! - myPrices[0]
+            myPrices.remove(at: 0)
+            filtrMyPrices(&myPrices)
+        }
+        
+        return result
     }
 }
--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -1,8 +1,111 @@
 import UIKit
 
 class MessageDecryptor: NSObject {
+       
+       func decryptMessage(_ message: String) -> String {
+           
+           if !message.contains("[") { return message }
+           guard message.count > 0 else { return "" }
+           
+           var tempMessage1 = message
+           
+           while tempMessage1.contains("[") {
+               changeString(value: &tempMessage1)
+           }
     
-    func decryptMessage(_ message: String) -> String {
-        return ""
-    }
+
+           return tempMessage1
+       }
+       
+       func changeString(value: inout String) {
+           let testMessage = value
+           let indexStart = testMessage.index(after: testMessage.firstIndex(of: "[")!)
+           
+           
+           var flag: Bool = true
+           var count1 = 1
+           for i in testMessage {
+               
+               
+               if i == "[" {
+                   count1 += 1
+               }
+               if i == "]" {
+                   count1 -= 2
+               }
+               if count1 == 3 {
+                   flag = true
+                   break
+               }
+               if count1 == 0 {
+                   flag = false
+                   break
+               }
+           }
+           
+           
+           var indexEnd : String.Index
+           if flag {
+               indexEnd = testMessage.index(before: testMessage.lastIndex(of: "]")!)
+           } else {
+               indexEnd = testMessage.index(before: testMessage.firstIndex(of: "]")!)
+           }
+           
+
+           
+           var resultString = ""
+
+           let middleString = testMessage[indexStart...indexEnd]
+           
+           var endMiddleString = ""
+
+           var firstString = ""
+           var lastString = ""
+           var countString = ""
+           
+           for i in testMessage {
+               if i != "[" && Int(String(i)) == nil {
+                   firstString.append(i)
+               } else if i != "[" && Int(String(i)) != nil {
+                   countString.append(i)
+               } else {
+                   break
+               }
+           }
+           if flag {
+               for i in testMessage.reversed() {
+                   if i != "]" {
+                       lastString.insert(i, at: lastString.startIndex)
+                   } else {
+                       break
+                   }
+               }
+           } else {
+               var temp = testMessage
+               for i in testMessage {
+                       if i != "]" {
+                           temp.remove(at: temp.startIndex)
+                       } else if i == "]" {
+                           temp.remove(at: temp.startIndex)
+                           break
+                       }
+               }
+               lastString = temp
+           }
+
+
+           if let i = Int(countString) {
+
+               if i >= 1 {
+                   for _ in 1...i {
+                      endMiddleString += String(middleString)
+                   }
+               }
+               resultString = String(firstString) + endMiddleString + String(lastString)
+           } else {
+               resultString = String(firstString) + String(middleString) + String(lastString)
+           }
+
+           value = resultString
+       }
 }
